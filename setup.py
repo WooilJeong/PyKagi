7a9b9ff74a37a8f3b04e2508a04c289fba83315c
정우일 <wooil@kakao.com>
2023-03-30 14:05:31 +0900

    fix

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Apache License 2.0",
         "Operating System :: OS Independent"
     ],
     install_requires=install_requires,
